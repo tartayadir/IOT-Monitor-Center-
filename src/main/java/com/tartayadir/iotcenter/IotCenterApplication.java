92dee2c399fd2966a00cd8e879b6594abe192454
Yaroslav B <tartayadirhello@gmail.com>
2024-12-29 18:42:20 +0100

    init commit
    - Added IOT part
    - Added MQTT in docker

---
+++ src/main/java/com/tartayadir/iotcenter/IotCenterApplication.java
@@ -0,0 +1,12 @@
+package com.tartayadir.iotcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotCenterApplication.class, args);
+    }
+}
---
+++ src/test/java/com/tartayadir/iotcenter/IotCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tartayadir.iotcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
